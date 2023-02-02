4ef6df0204cc2dccda15b15366ab32cadf2cc88a
alexandra.boangiu <alexandra.boangiu98@gmail.com>
2023-02-02 23:23:48 +0200

    route

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/test', function(req, res, next) {
+  let test = {title: 'Test object'};
+  res.send(test);
+});
 
 module.exports = router;
