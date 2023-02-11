7b8e930a0ce7e5a5b3c6e13f237cf528aaf833cf
alexandra.boangiu <alexandra.boangiu98@gmail.com>
2023-02-11 23:28:54 +0200

    update users route

--- routes/users.js
+++ routes/users.js
@@ -14,10 +14,70 @@ router.post('/register', async function(req, res, next) {
       var firstName = req.body.firstName;
       var lastName = req.body.lastName;
       
-      await connectiondb.query(`INSERT INTO Users(first_name, last_name, email, password, role) VALUES ('${firstName}', '${lastName}', '${email}', '${password}', 'standard')`, (err, rows, fields) => {
+      await connectiondb.query(`SELECT * FROM Users WHERE email = '${email}'`, async (err, rows, fields) => {
         if (err) throw err
-        console.log(rows)
+        if(rows.length === 0) {
+          await connectiondb.query(`INSERT INTO Users(first_name, last_name, email, password, role) VALUES ('${firstName}', '${lastName}', '${email}', '${password}', 'standard')`, (err, rows, fields) => {
+            if (err) throw err
+            let idUser = "" + rows.insertId;
+            res.send(idUser);
+          })
+        } else {
+          res.send("duplicate");
+        }
       })
+    })
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// router.post('/register', async function(req, res, next) {
+//   try {
+//     bcrypt.hash(req.body.password, saltRounds)
+//     .then( async hash => {
+//       req.body.password = hash;
+//       var email = req.body.email;
+//       var password = req.body.password;
+//       var firstName = req.body.firstName;
+//       var lastName = req.body.lastName;
+      
+//       await connectiondb.query(`INSERT INTO Users(first_name, last_name, email, password, role) VALUES ('${firstName}', '${lastName}', '${email}', '${password}', 'standard')`, (err, rows, fields) => {
+//         if (err) throw err
+//         console.log(rows)
+//       })
+//     })
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+
+
+router.post('/login', async function(req, res, next) {
+  let response = {};
+  try {
+    var email = req.body.email;
+    var password = req.body.password;
+    await connectiondb.query(`SELECT * FROM Users WHERE email = '${email}'`, (err, rows, fields) => {
+      if (err) throw err
+      if(rows.length == 0) {
+        response.status = "unfound";
+        res.send(response);
+      } else {
+        console.log(password)
+        bcrypt.compare(password, rows[0].password)
+        .then(comparationResult => {
+          console.log(comparationResult)
+          if(comparationResult) {
+            response.status = "success";
+            response.idUser = rows[0].id_user;
+            res.send(response);
+          } else {
+            response.status = "mismatch";
+            res.send(response);
+          }
+         })
+      }
     })
   } catch (error) {
     console.log(error);
