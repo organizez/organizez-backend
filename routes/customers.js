7206709af9d467fd5ef7170f995ec6e24ed7d8f5
alexandra.boangiu <alexandra.boangiu98@gmail.com>
2023-03-14 12:30:17 +0200

    add companyEmai

--- routes/customers.js
+++ routes/customers.js
@@ -14,7 +14,7 @@ router.get('/getCustomersNumber/', async function(req, res, next) {
 router.get('/getAllCustomers/:iteration', async function(req, res, next) {
     let customers = [];
     let iteration = req.params.iteration;
-    await connectiondb.query(`select c.id_customer, c.name_company, CONCAT(first_name_representative, ' ', last_name_representative) AS name_representative, c.email_representative, c.phone_representative, c.subscription_type, cs.id_customer_service, cs.name, cs.location, cs.website, cs.phone, cs.short_description, cs.long_description, cs.image1, cs.image2, cs.image3, cs.image4, cs.image5, cs.image6, cs.image7, cs.image8, cs.image9, cs.image10, cs.image11, cs.image12, cs.image13, cs.image14, cs.image15, cs.image16, cs.image17, cs.image18, cs.image19, cs.image20, cs.minimum_capacity, cs.maximum_capacity, cs.number_hall, sc.category, ct.city from Customers c, Customers_Services cs, Services_Categories sc, Cities ct where c.id_customer = cs.id_customer and cs.id_category = sc.id_category and cs.id_city = ct.id_city LIMIT 15 OFFSET ${iteration};`, (err, rows, fields) => {
+    await connectiondb.query(`select c.id_customer, c.name_company, CONCAT(first_name_representative, ' ', last_name_representative) AS name_representative, c.email_representative, c.phone_representative, c.subscription_type, cs.id_customer_service, cs.name, cs.location, cs.website, cs.company_email, cs.phone, cs.short_description, cs.long_description, cs.image1, cs.image2, cs.image3, cs.image4, cs.image5, cs.image6, cs.image7, cs.image8, cs.image9, cs.image10, cs.image11, cs.image12, cs.image13, cs.image14, cs.image15, cs.image16, cs.image17, cs.image18, cs.image19, cs.image20, cs.minimum_capacity, cs.maximum_capacity, cs.number_hall, sc.category, ct.city from Customers c, Customers_Services cs, Services_Categories sc, Cities ct where c.id_customer = cs.id_customer and cs.id_category = sc.id_category and cs.id_city = ct.id_city LIMIT 15 OFFSET ${iteration};`, (err, rows, fields) => {
         if (err) throw err
         customers = rows;
         res.send(customers);
@@ -25,7 +25,7 @@ router.get('/getAllCustomersByCategory/:id_category/:iteration', async function(
     let customers = [];
     let idCategory = req.params.idCategory;
     let iteration = req.params.iteration;
-    await connectiondb.query(`select c.id_customer, c.name_company, CONCAT(first_name_representative, ' ', last_name_representative) AS name_representative, c.email_representative, c.phone_representative, c.subscription_type, cs.id_customer_service, cs.name, cs.location, cs.website, cs.phone, cs.short_description, cs.long_description, cs.image1, cs.image2, cs.image3, cs.image4, cs.image5, cs.image6, cs.image7, cs.image8, cs.image9, cs.image10, cs.image11, cs.image12, cs.image13, cs.image14, cs.image15, cs.image16, cs.image17, cs.image18, cs.image19, cs.image20, cs.minimum_capacity, cs.maximum_capacity, cs.number_hall, sc.category, ct.city from Customers c, Customers_Services cs, Services_Categories sc, Cities ct where c.id_customer = cs.id_customer and cs.id_category = sc.id_category and cs.id_city = ct.id_city and cs.id_category = ${idCategory} LIMIT 15 OFFSET ${iteration};`, (err, rows, fields) => {
+    await connectiondb.query(`select c.id_customer, c.name_company, CONCAT(first_name_representative, ' ', last_name_representative) AS name_representative, c.email_representative, c.phone_representative, c.subscription_type, cs.id_customer_service, cs.name, cs.location, cs.website, cs.company_email, cs.phone, cs.short_description, cs.long_description, cs.image1, cs.image2, cs.image3, cs.image4, cs.image5, cs.image6, cs.image7, cs.image8, cs.image9, cs.image10, cs.image11, cs.image12, cs.image13, cs.image14, cs.image15, cs.image16, cs.image17, cs.image18, cs.image19, cs.image20, cs.minimum_capacity, cs.maximum_capacity, cs.number_hall, sc.category, ct.city from Customers c, Customers_Services cs, Services_Categories sc, Cities ct where c.id_customer = cs.id_customer and cs.id_category = sc.id_category and cs.id_city = ct.id_city and cs.id_category = ${idCategory} LIMIT 15 OFFSET ${iteration};`, (err, rows, fields) => {
         if (err) throw err
         customers = rows;
         res.send(customers);
@@ -36,7 +36,7 @@ router.get('/getCustomerById/:idCustomer', async function(req, res, next) {
     let customer = {};
     console.log(req.params)
     let idCustomer = req.params.idCustomer;
-    await connectiondb.query(`select c.id_customer, c.name_company, first_name_representative, last_name_representative, c.email_representative, c.phone_representative, c.subscription_type, cs.id_customer_service, cs.name, cs.location, cs.website, cs.phone, cs.short_description, cs.long_description, cs.image1, cs.image2, cs.image3, cs.image4, cs.image5, cs.image6, cs.image7, cs.image8, cs.image9, cs.image10, cs.image11, cs.image12, cs.image13, cs.image14, cs.image15, cs.image16, cs.image17, cs.image18, cs.image19, cs.image20, cs.minimum_capacity, cs.maximum_capacity, cs.number_hall, sc.id_category, sc.category, ct.id_city, ct.city from Customers c, Customers_Services cs, Services_Categories sc, Cities ct where c.id_customer = cs.id_customer and cs.id_category = sc.id_category and cs.id_city = ct.id_city and cs.id_customer_service = ${idCustomer};`, (err, rows, fields) => {
+    await connectiondb.query(`select c.id_customer, c.name_company, first_name_representative, last_name_representative, c.email_representative, c.phone_representative, c.subscription_type, cs.id_customer_service, cs.name, cs.location, cs.website, cs.company_email, cs.phone, cs.short_description, cs.long_description, cs.image1, cs.image2, cs.image3, cs.image4, cs.image5, cs.image6, cs.image7, cs.image8, cs.image9, cs.image10, cs.image11, cs.image12, cs.image13, cs.image14, cs.image15, cs.image16, cs.image17, cs.image18, cs.image19, cs.image20, cs.minimum_capacity, cs.maximum_capacity, cs.number_hall, sc.id_category, sc.category, ct.id_city, ct.city from Customers c, Customers_Services cs, Services_Categories sc, Cities ct where c.id_customer = cs.id_customer and cs.id_category = sc.id_category and cs.id_city = ct.id_city and cs.id_customer_service = ${idCustomer};`, (err, rows, fields) => {
         if (err) throw err
         customer = rows[0];
         res.send(customer);
@@ -55,6 +55,7 @@ router.post('/addCustomer', async function(req, res, next) {
     var location = req.body.location;
     var website = req.body.website;
     var phone = req.body.phone;
+    var companyEmail = req.body.companyEmail;
     var shortDescription = req.body.shortDescription;
     var longDescription = req.body.longDescription;
     var numberHall = req.body.numberHall;
@@ -86,7 +87,7 @@ router.post('/addCustomer', async function(req, res, next) {
     await connectiondb.query(`INSERT INTO Customers(last_name_representative, first_name_representative, phone_representative, email_representative, name_company, subscription_type) VALUES ('${lastNameRepresentative}', '${firstNameRepresentative}', '${phoneRepresentative}', '${emailRepresentative}', '${company}', '${subscriptionType}')`, async(err, rows, fields) => {
         if (err) throw err;
         let idCustomer = "" + rows.insertId;
-        await connectiondb.query(`INSERT INTO Customers_Services(name, location, website, phone, short_description, long_description, number_hall, minimum_capacity, maximum_capacity, image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13, image14, image15, image16, image17, image18, image19, image20, id_customer, id_category, id_city) VALUES ('${name}', '${location}', '${website}', '${phone}', '${shortDescription}', '${longDescription}', '${numberHall}', '${minimumCapacity}', '${maximumCapacity}', '${image1}', '${image2}', '${image3}', '${image4}', '${image5}', '${image6}','${image7}', '${image8}', '${image9}', '${image10}',  '${image11}', '${image12}', '${image13}', '${image14}', '${image15}', '${image16}','${image17}', '${image18}', '${image19}', '${image20}','${idCustomer}', '${idCategory}', '${idCity}')`, async(err, rows, fields) => {
+        await connectiondb.query(`INSERT INTO Customers_Services(name, location, website, phone, company_email, short_description, long_description, number_hall, minimum_capacity, maximum_capacity, image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13, image14, image15, image16, image17, image18, image19, image20, id_customer, id_category, id_city) VALUES ('${name}', '${location}', '${website}', '${phone}', '${companyEmail}', '${shortDescription}', '${longDescription}', '${numberHall}', '${minimumCapacity}', '${maximumCapacity}', '${image1}', '${image2}', '${image3}', '${image4}', '${image5}', '${image6}','${image7}', '${image8}', '${image9}', '${image10}',  '${image11}', '${image12}', '${image13}', '${image14}', '${image15}', '${image16}','${image17}', '${image18}', '${image19}', '${image20}','${idCustomer}', '${idCategory}', '${idCity}')`, async(err, rows, fields) => {
             if (err) throw err;
             let idCustomerService = "" + rows.insertId;
             if (selectedFacilitiesOptions.length > 0) {
@@ -96,7 +97,6 @@ router.post('/addCustomer', async function(req, res, next) {
                     })
                 }
             }
-
             res.send("succes");
         })
     })
@@ -116,6 +116,7 @@ router.put('/updateCustomer', async function(req, res, next) {
     var location = req.body.location;
     var website = req.body.website;
     var phone = req.body.phone;
+    var companyEmail = req.body.companyEmail;
     var shortDescription = req.body.shortDescription;
     var longDescription = req.body.longDescription;
     var numberHall = req.body.numberHall;
@@ -146,7 +147,7 @@ router.put('/updateCustomer', async function(req, res, next) {
     var selectedFacilitiesOptions = req.body.selectedFacilitiesOptions;
     await connectiondb.query(`UPDATE Customers SET last_name_representative = '${lastNameRepresentative}', first_name_representative = '${firstNameRepresentative}', phone_representative = '${phoneRepresentative}', email_representative = '${emailRepresentative}', name_company = '${company}', subscription_type = '${subscriptionType}' WHERE id_customer = '${idCustomer}';`, async(err, rows, fields) => {
         if (err) throw err;
-        await connectiondb.query(`UPDATE Customers_Services SET name = '${name}', location = '${location}', website = '${website}', phone = '${phone}', short_description = '${shortDescription}', long_description = '${longDescription}', number_hall = '${numberHall}', minimum_capacity =  '${minimumCapacity}', maximum_capacity = '${maximumCapacity}', image1 = '${image1}', image2 = '${image2}', image3 = '${image3}', image4 = '${image4}', image5 = '${image5}', image6 = '${image6}', image7 = '${image7}', image8 = '${image8}', image9 = '${image9}', image10 = '${image10}', image11 = '${image11}', image12 = '${image12}', image13 = '${image13}', image14 = '${image14}', image15 = '${image15}', image16 = '${image16}', image17 = '${image17}', image18 = '${image18}', image19 = '${image19}', image20 = '${image20}', id_customer = '${idCustomer}', id_category = '${idCategory}', id_city = '${idCity}' WHERE id_customer_service = '${idCustomerService}'`, async(err, rows, fields) => {
+        await connectiondb.query(`UPDATE Customers_Services SET name = '${name}', location = '${location}', website = '${website}', phone = '${phone}', company_email = '${companyEmail}', short_description = '${shortDescription}', long_description = '${longDescription}', number_hall = '${numberHall}', minimum_capacity =  '${minimumCapacity}', maximum_capacity = '${maximumCapacity}', image1 = '${image1}', image2 = '${image2}', image3 = '${image3}', image4 = '${image4}', image5 = '${image5}', image6 = '${image6}', image7 = '${image7}', image8 = '${image8}', image9 = '${image9}', image10 = '${image10}', image11 = '${image11}', image12 = '${image12}', image13 = '${image13}', image14 = '${image14}', image15 = '${image15}', image16 = '${image16}', image17 = '${image17}', image18 = '${image18}', image19 = '${image19}', image20 = '${image20}', id_customer = '${idCustomer}', id_category = '${idCategory}', id_city = '${idCity}' WHERE id_customer_service = '${idCustomerService}'`, async(err, rows, fields) => {
             if (err) throw err;
             await connectiondb.query(`DELETE FROM Facilities_Options WHERE id_customer_service = '${idCustomerService}'`, async(err, rows, fields) => {
                 if (err) throw err;
